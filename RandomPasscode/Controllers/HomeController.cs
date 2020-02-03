92e9d245b9052ca97bd337252ddc23f2afd008d9
andrew-s95 <56849472+andrew-s95@users.noreply.github.com>
2020-02-03 12:02:46 -0800

    RandomPasscode
    Generating random passcodes with ASP.NET

---
+++ RandomPasscode/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RandomPasscode.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace RandomPasscode.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            int? count = HttpContext.Session.GetInt32("count");
+            if (count == null)
+            {
+                HttpContext.Session.SetInt32("count", 1);
+            }
+            else
+            {
+                count += 1;
+                HttpContext.Session.SetInt32("count", (int)count);
+            }
+
+            string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            string passcode = "";
+            Random rand = new Random();
+            for (int i = 1; i <= 14; i++)
+            {
+                int x = rand.Next(0,36);
+                passcode += characters[x];
+            }
+            ViewBag.Passcode = passcode;
+            ViewBag.Count = count;
+            return View();
+        }
+        [HttpGet("/clear")]
+        public IActionResult Clear()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
